45809e859af74f5030cd572aa50234e5c7c4f322
hyteer <hyteer@qq.com>
2017-03-03 01:09:00 +0800

    finished tutorial1

--- djrest/example/urls.py
+++ djrest/example/urls.py
@@ -16,6 +16,8 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/$', views.hello_world),
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^users/$', views.ListUsers.as_view()),
 ]
--- djrest/example/views.py
+++ djrest/example/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
 from .serializers import UserSerializer, GroupSerializer
+
+
+@api_view()
+@permission_classes((permissions.AllowAny,))
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,3 +28,21 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class ListUsers(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    #authentication_classes = (authentication.TokenAuthentication,)
+    #permission_classes = (permissions.IsAdminUser,)
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
